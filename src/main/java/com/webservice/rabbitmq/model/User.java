1c7e75baec901839f47434fb7307b66e2bdadada
Célian Frasca <celian.frasca@gmail.com>
2023-11-24 11:42:12 +0100

    add docker and jar

--- src/main/java/com/webservice/rabbitmq/model/User.java
+++ src/main/java/com/webservice/rabbitmq/model/User.java
@@ -1,2 +1,14 @@
-package com.webservice.rabbitmq.model;public class User {
+package com.webservice.rabbitmq.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class User implements Serializable {
+    private String name;
+    private String email;
+    private String verificationToken;
 }
